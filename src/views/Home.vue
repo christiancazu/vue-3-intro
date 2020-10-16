fd440969b1b38fa47c449ce107812fca43ddbbac
Christian Carrillo <christiancazu@gmail.com>
2020-10-16 16:18:58 -0500

    computed & watchers

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,9 @@
 <template>
 <section class="p-4">
-  <h4 class="h4 text-center">Ref vs Reactive</h4>
+  <h4 class="h4 text-center">Computed & Watchers</h4>
 
-  <div>{{ value }}</div>
+  <input class="form-control">
 
-  <button
-    class="btn btn-primary"
-    @click="increment"
-  >
-    increment
-  </button>
 </section>
 </template>
 
@@ -17,26 +11,17 @@
 export default {
   name: 'Home',
 
-  data () {
-    return {
-      value: 0
-    }
-  },
-
   setup () {
-  },
 
-  methods: {
-    increment () {
-      this.value++
+    return {
     }
   }
 }
 
 /**
- * ref (unwrap)
- * reactive (destructuring)
- * toRefs
- * toRef
+ * computed (get, set)
+ * watch (array)
+ * watchEffect (async API: 'https://rickandmortyapi.com/api/character/id')
+ * readonly
  */
 </script>
