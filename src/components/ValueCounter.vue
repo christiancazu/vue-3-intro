60221749f4e6b4b44a13ebf000a2c6f432fb7de2
Christian Carrillo <christiancazu@gmail.com>
2020-10-16 14:18:19 -0500

    setup

---
+++ src/components/ValueCounter.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <h1 class="h1">
+    counter value: {{ value }}
+  </h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'ValueCounter',
+
+  props: {
+    value: {
+      type: Number,
+      default: 0
+    }
+  },
+
+  setup () {
+  }
+}
+/**
+ * return
+ * props,
+ * context { attrs, emit, slots },
+ */
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,36 @@
+
 <template>
 <section>
+  <ValueCounter :value="value" />
+
+  <button
+    class="btn btn-primary"
+    @click="increment">
+    increment
+  </button>
 </section>
 </template>
 
 <script>
+import ValueCounter from '@/components/ValueCounter'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+
+  components: {
+    ValueCounter
+  },
+
+  data () {
+    return {
+      value: 0
+    }
+  },
+
+  methods: {
+    increment () {
+      this.value++
+    }
+  }
 }
 </script>
