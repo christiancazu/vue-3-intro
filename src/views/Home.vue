1303e3b15a93efb93c9f95e2002a1da5894e3bc0
Christian Carrillo <christiancazu@gmail.com>
2020-10-20 14:58:18 -0500

    css variables

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,41 @@
 <template>
 <section class="p-4">
   <h4 class="h4 text-center">Vue 3</h4>
+
+  <button
+    class="btn btn-success"
+    @click="modifyCSSVariables"
+  >
+    modifyCSSVariables
+  </button>
+
+  <div class="my-text my-2">MY TEXT</div>
 </section>
 </template>
 
 <script setup>
+import { ref, computed } from 'vue'
 
+const size = ref(10)
 
+function modifyCSSVariables () {
+  size.value++
+}
+
+const computedSize = computed(() => size.value + 'px')
+
+export {
+  modifyCSSVariables,
+  computedSize
+}
 /**
  * multiple nodes
+ * '#000000'.replace(/0/g, () => (~~(Math.random() * 16)).toString(16))
  */
 </script>
 
-<style vars>
+<style vars="{ computedSize }">
+.my-text {
+ font-size: var(--computedSize);
+}
 </style>
