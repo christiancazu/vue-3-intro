fec1e246d614b9c8564dee15c6039ff1d897894d
Christian Carrillo <christiancazu@gmail.com>
2020-10-23 16:58:49 -0500

    breaking changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
 <section class="p-4">
-  <h4 class="h4 text-center">Vue 3</h4>
+  <h4 class="h4 text-center">Vue 3 - Breaking changes</h4>
 </section>
 </template>
 
@@ -8,7 +8,35 @@
 
 
 /**
- * multiple nodes
+ * sync modifier
+ * key in template
+ * multiple ref
+ *
+ *
+ *
+  <div
+    v-for="(number, index) in 8"
+    :key="index"
+    :ref="setItemRef"
+  >
+    asdas
+  </div>
+
+
+ * let itemRefs = []
+
+  const setItemRef = el => {
+    itemRefs.push(el)
+  }
+
+  onMounted(() => {
+    console.log(itemRefs)
+  })
+ *
+  *
+  *
+  * mitt replace event bus
+
  */
 </script>
 
