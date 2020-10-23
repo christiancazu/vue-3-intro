ba2e7e1027553f31a9c949fd693de49fb539ed9a
Christian Carrillo <christiancazu@gmail.com>
2020-10-23 18:12:43 -0500

    advance

---
+++ src/components/Input.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="hello">
+  <input
+    class="form-control"
+    :value="name"
+    @input="$emit('update:name', $event.target.value)"
+  >
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+
+  props: {
+    name: {
+      type: String,
+      default: ''
+    }
+  },
+
+  emits: {
+    'update:name': (val) => {
+      if (val.length > 5) {
+        alert('no es valido')
+        return false
+      }
+      return true
+    }
+  }
+
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,32 @@
 <template>
 <section class="p-4">
   <h4 class="h4 text-center">Vue 3 - Multiple v-model</h4>
+
+  {{ name }}
+
+  <Input v-model:name="name" />
 </section>
 </template>
 
-<script setup>
+<script>
+const { ref } = require('vue')
+
+import Input from '@/components/Input'
+export default {
+  components: {
+    Input
+  },
+
+  setup () {
+    const name = ref('abc')
+
+    return {
+      name,
+      Input
+    }
+  }
+}
+
 
 
 /**
