591cfe3eb6f4e7682c28964dad6e365244d54ff3
Christian Carrillo <christiancazu@gmail.com>
2020-10-23 17:31:17 -0500

    advance

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,38 @@
 <section class="p-4">
   <h4 class="h4 text-center">Vue 3 - Teleport</h4>
 
-  <Teleport>
+  <button
+    class="btn btn-primary"
+    @click="toggleDestination"
+  >
+    toggle destination
+  </button>
+
+  <Teleport :to="destination">
     <span class="badge badge-success">MY CONTENT</span>
   </Teleport>
 </section>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   setup () {
+    const destination = ref('#footer')
+
+    function toggleDestination () {
+      if (destination.value === '#footer') {
+        destination.value = '#sidebar'
+      } else {
+        destination.value = '#footer'
+      }
+    }
+
+    return {
+      destination,
+      toggleDestination
+    }
   }
 }
 
