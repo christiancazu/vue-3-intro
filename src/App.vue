3e8fcd3d3cf07475a0895dff7f5da75b4104e874
Christian Carrillo <christiancazu@gmail.com>
2020-10-23 15:25:45 -0500

    teleport

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
 <main>
+  <section class="fixed-right bg-warning">
+    <h6 class="h6">teleport slot sidebar</h6>
+    <div id="sidebar" />
+  </section>
+
   <h1 class="h1 text-center text-title">Composition API - Vue 3</h1>
 
   <!-- <RouterLink to="/">Home</RouterLink> |
   <RouterLink to="/about">About</RouterLink> -->
   <RouterView />
+
+  <section class="fixed-bottom bg-info">
+    <h6 class="h6">teleport slot footer</h6>
+    <div id="footer" />
+  </section>
 </main>
 </template>
 
@@ -18,5 +28,19 @@ main {
   text-decoration: underline;
   color: greenyellow;
   font-weight: bold;
+}
+
+.fixed-bottom {
+  height: 5rem;
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+}
+
+.fixed-right {
+  height: 100vh;
+  width: 8rem;
+  position: fixed;
+  right: 0;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,20 @@
 <template>
 <section class="p-4">
-  <h4 class="h4 text-center">Vue 3</h4>
+  <h4 class="h4 text-center">Vue 3 - Teleport</h4>
+
+  <Teleport>
+    <span class="badge badge-success">MY CONTENT</span>
+  </Teleport>
 </section>
 </template>
 
-<script setup>
-
+<script>
+export default {
+  setup () {
+  }
+}
 
 /**
- * multiple nodes
+ * teleport
  */
 </script>
-
-<style vars>
-</style>
