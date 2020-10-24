2c4520de08312aecf4e177700bb450fa5f803def
Christian Carrillo <christiancazu@gmail.com>
2020-10-23 19:05:57 -0500

    advance

---
+++ src/components/Foo.vue
@@ -0,0 +1,33 @@
+<template>
+<h1 class="h1 text-primary">component</h1>
+
+<button @click="emitByEventBus">emit</button>
+</template>
+
+<script>
+import BusEvent from '@/bus-event'
+
+export default {
+  props: {
+    number: {
+      type: Number,
+      default: 0
+    }
+  },
+
+  setup (props) {
+    function sayFoo () {
+      console.log('sayy foo', props.number)
+    }
+
+    function emitByEventBus () {
+      BusEvent.emit('foo', 5)
+    }
+
+    return {
+      sayFoo,
+      emitByEventBus
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,87 @@
 <template>
 <section class="p-4">
   <h4 class="h4 text-center">Vue 3 - Breaking changes</h4>
+
+  <!-- <template
+    v-for="(number) in 8"
+    :key="number"
+  >
+    <div v-if="false">{{ number }}</div>
+    <div v-else>{{ number * 2 }}</div>
+  </template> -->
+
+  <!-- <button
+    class="btn btn-primary"
+    @click="callToChildMethod"
+  >
+    callToChildMethod
+  </button> -->
+
+  <!-- <Foo
+    v-for="(number, index) in 8"
+    :key="index"
+    :ref="setRefItems"
+    :number="index"
+  >
+    COMPONENT
+  </Foo> -->
+
+  <Foo />
 </section>
 </template>
 
-<script setup>
+<script>
+import { onMounted } from 'vue'
+import Foo from '@/components/Foo'
+
+import BusEvent from '@/bus-event'
+
+
+// const EventBus = new Vue()
+
+// EventBus.$on('listener', (params) => {
+//   console.warn(object)
+// })
+
+// EventBus.$emit('listener', 5)
+
+
+export default {
+  components: {
+    Foo
+  },
+
+  setup () {
+    BusEvent.on('foo', e => console.log('BUS_EVENT emited', e))
+
+    let refItems = []
+
+    function setRefItems (ref) {
+      refItems.push(ref)
+    }
+
+    onMounted(() => {
+      console.warn(refItems)
+    })
+
+    function callToChildMethod () {
+      refItems[5].sayFoo()
+    }
+
+    return {
+      setRefItems,
+      callToChildMethod
+    }
+  }
+}
 
 
 /**
- * sync modifier
- * key in template
+ * x sync modifier
+ * x key in template
  * multiple ref
  *
- *
- *
-  <div
-    v-for="(number, index) in 8"
-    :key="index"
-    :ref="setItemRef"
-  >
-    asdas
-  </div>
-
-
- * let itemRefs = []
-
-  const setItemRef = el => {
-    itemRefs.push(el)
-  }
-
-  onMounted(() => {
-    console.log(itemRefs)
-  })
- *
-  *
-  *
-  * mitt replace event bus
-
+ * mitt replace event bus
  */
 </script>
 
